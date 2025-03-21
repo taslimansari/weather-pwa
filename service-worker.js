5eb90d24c3e3e9e5387f6cb158956899deec11a3
Student <student@rcoesrv.com>
2025-03-21 10:52:29 +0530

    Initial commit

---
+++ service-worker.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'weather-app-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/script.js',
+  '/icons/icon-192x192.png',
+  '/icons/icon-512x512.png',
+  '/background.jpg'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
